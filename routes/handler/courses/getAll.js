7e6121fb25bf6482d533ecb2a7d8958e24804a7a
balinux <ryojuniyantara@gmail.com>
2022-12-31 07:03:44 +0800

    API GATEWAY - course service - update path response

--- routes/handler/courses/getAll.js
+++ routes/handler/courses/getAll.js
@@ -1,7 +1,7 @@
 const apiAdapter = require('../../apiAdapters');
 
 const {
-    URL_SERVICE_COURSE
+    URL_SERVICE_COURSE, HOSTNAME
 } = process.env;
 
 const api = apiAdapter(URL_SERVICE_COURSE);
@@ -9,13 +9,42 @@ const api = apiAdapter(URL_SERVICE_COURSE);
 module.exports = async (req, res) => {
     try {
         console.log("try");
-        const mentors = await api.get('/api/mentors');
+        const courses = await api.get('/api/courses', {
+            params: {
+                ...req.query,
+                status: 'published'
+            }
+        });
 
-        return res.json(mentors.data);
+        const coursesData = courses.data;
+
+        // mengambil  data firstpage dari json
+        const firstPage = coursesData.data.first_page_url.split('?').pop();
+        const lastPage = coursesData.data.last_page_url.split('?').pop();
+
+        // menginject data baru dengan menggunakan hostname api gateway dan mendapatkan parameter terakhir
+        coursesData.data.first_page_url = `${HOSTNAME}/courses?${firstPage}`
+        coursesData.data.last_page_url = `${HOSTNAME}/courses?${lastPage}`
+
+        // mengubah next_page_url
+        if (coursesData.data.next_page_url) {
+            const nextPage = coursesData.data.next_page_url.split('?').pop();
+            coursesData.data.next_page_url = `${  HOSTNAME}/courses?${nextPage}`
+        }
+        
+        // mengubah prev_page_url
+        if (coursesData.data.prev_page_url) {
+            const prevPage = coursesData.data.prev_page_url.split('?').pop();
+            coursesData.data.prev_page_url = `${HOSTNAME}/courses?${prevPage}`
+        }
+
+        coursesData.data.path = `${HOSTNAME}/courses`
+
+        return res.json(coursesData);
     } catch (error) {
 
         if (error.code === 'ECONNREFUSED') {
-            return res.status(500).json({ status: 'error', message: 'service mentor unavailable' });
+            return res.status(500).json({ status: 'error', message: 'service course unavailable' });
         }
 
         const { status, data } = error.response;
