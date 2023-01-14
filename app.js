e38477dfd61be9a1a56840cfa7f478b99b6c533e
balinux <ryojuniyantara@gmail.com>
2023-01-14 20:59:26 +0800

    update: permission

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const coursesRouter = require('./routes/courses');
-const mediaRouter = require('./routes/media');
-const orderPaymentsRouter = require('./routes/orderPayments');
-const refreshTokensRouter = require('./routes/refreshTokens');
-const mentorsRouter = require('./routes/mentors');
-const chaptersRouter = require('./routes/chapters');
-const lessonsRouter = require('./routes/lessons');
-const imageCoursesRouter = require('./routes/imageCourses');
-const myCoursesRouter = require('./routes/myCourses');
-const reviewsRouter = require('./routes/reviews');
-const webhookRouter = require('./routes/webhook');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const coursesRouter = require("./routes/courses");
+const mediaRouter = require("./routes/media");
+const orderPaymentsRouter = require("./routes/orderPayments");
+const refreshTokensRouter = require("./routes/refreshTokens");
+const mentorsRouter = require("./routes/mentors");
+const chaptersRouter = require("./routes/chapters");
+const lessonsRouter = require("./routes/lessons");
+const imageCoursesRouter = require("./routes/imageCourses");
+const myCoursesRouter = require("./routes/myCourses");
+const reviewsRouter = require("./routes/reviews");
+const webhookRouter = require("./routes/webhook");
 
 // middleware verify token
-const verifyToken = require('./middlewares/veryfyToken')
+const verifyToken = require("./middlewares/veryfyToken");
+const can = require("./middlewares/permission");
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/courses', coursesRouter);
-app.use('/media', mediaRouter);
-app.use('/orders', verifyToken, orderPaymentsRouter);
-app.use('/refresh-tokens', refreshTokensRouter);
-app.use('/mentors', verifyToken, mentorsRouter);
-app.use('/chapters', verifyToken, chaptersRouter);
-app.use('/lessons', verifyToken, lessonsRouter);
-app.use('/image-courses', verifyToken, imageCoursesRouter);
-app.use('/my-courses', verifyToken, myCoursesRouter);
-app.use('/reviews', verifyToken, reviewsRouter);
-app.use('/webhook', webhookRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/courses", coursesRouter);
+app.use("/media", verifyToken, can("admin", "student"), mediaRouter);
+app.use("/orders", verifyToken, can("admin", "student"), orderPaymentsRouter);
+app.use("/refresh-tokens", refreshTokensRouter);
+app.use("/mentors", verifyToken, can("admin"), mentorsRouter);
+app.use("/chapters", verifyToken, can("admin"), chaptersRouter);
+app.use("/lessons", verifyToken, can("admin"), lessonsRouter);
+app.use("/image-courses", verifyToken, can("admin"), imageCoursesRouter);
+app.use("/my-courses", verifyToken, can("admin", "student"), myCoursesRouter);
+app.use("/reviews", verifyToken, can("admin", "student"), reviewsRouter);
+app.use("/webhook", webhookRouter);
 
 module.exports = app;
--- routes/courses.js
+++ routes/courses.js
@@ -1,18 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const { APP_NAME } = process.env;
 
-const coursesHandler = require('./handler/courses')
+const coursesHandler = require("./handler/courses");
 
 // middleware verify token
-const verifyToken = require('../middlewares/veryfyToken')
+const verifyToken = require("../middlewares/veryfyToken");
+const can = require("../middlewares/permission");
 
 /* GET users listing. */
-router.get('/', coursesHandler.getAll);
-router.get('/:id', coursesHandler.get);
-router.post('/', verifyToken, coursesHandler.create);
-router.put('/:id', verifyToken, coursesHandler.update);
-router.delete('/:id', verifyToken, coursesHandler.destroy);
+router.get("/", coursesHandler.getAll);
+router.get("/:id", coursesHandler.get);
+router.post("/", verifyToken, can("admin"), coursesHandler.create);
+router.put("/:id", verifyToken, can("admin"), coursesHandler.update);
+router.delete("/:id", verifyToken, can("admin"), coursesHandler.destroy);
 
 module.exports = router;
