277f12f55e56c81056e5b3e1b083f7055010b3cb
balinux <ryojuniyantara@gmail.com>
2023-01-18 12:50:21 +0800

    update: morgan

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const can = require("./middlewares/permission");
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger("combined"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
